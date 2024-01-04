8108dddde73c81e3c24a3e3489c72dd00c1191fe
Quentin Rousseau <contact@quent.in>
2024-01-03 21:59:40 -0800

    Add specs for callback_url + fixes

--- lib/omniauth/strategies/service_now.rb
+++ lib/omniauth/strategies/service_now.rb
@@ -78,7 +78,7 @@ module OmniAuth
       end
 
       def callback_url
-        full_host + script_name + callback_path
+        options.redirect_url || (full_host + callback_path)
       end
     end
   end
